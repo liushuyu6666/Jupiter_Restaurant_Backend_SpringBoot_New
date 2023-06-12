a8ddb8234218ac4e3be320edbe832dafdad9624c
shuyu liu <liushuyu6666@gmail.com>
2023-06-12 00:12:34 -0400

    add Auth and its dependencies

--- src/main/java/com/jays/demo/DemoApplication.java
+++ src/main/java/com/jays/demo/DemoApplication.java
@@ -2,9 +2,10 @@ package com.jays.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 @EnableMongoAuditing
 public class DemoApplication {
 
